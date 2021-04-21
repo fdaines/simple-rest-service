495459c7a8c7253cd3c10a01bf388cc781a15c57
Francisco Daines <francisco.daines@walmart.com>
2021-04-21 17:26:06 -0400

    Fixes duplicated dates

--- infrastructure/promotions_handler_test.go
+++ infrastructure/promotions_handler_test.go
@@ -33,14 +33,14 @@ func (tgr *RepositoryStub) GetPromotions() ([]*model.Promotion, error) {
 		&model.Promotion{
 			Title: "Promotion 3",
 			Description: "Brief description of promotion 3",
-			StartDate: "2021-03-28",
+			StartDate: "2021-03-29",
 			EndDate: "2021-05-28",
 			IsActive: true,
 		},
 		&model.Promotion{
 			Title: "Promotion 4",
 			Description: "Brief description of promotion 4",
-			StartDate: "2021-03-28",
+			StartDate: "2021-03-30",
 			EndDate: "2021-05-28",
 			IsActive: true,
 		},
